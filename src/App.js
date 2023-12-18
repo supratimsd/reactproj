1c49a7212226a475e301935b058a6a7e2adb7357
supratimsd <supratim.dey.21@aot.edu.in>
2023-12-18 23:32:03 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+ 
 
 function App() {
+  let time=new Date().toLocaleTimeString();
+  const [ctime,setcTime]= useState(time);
+  const updateTime = ()=>{
+    let time=new Date().toLocaleTimeString();
+    setcTime(time);
+    
+  }
+  setInterval(updateTime,1000);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="container">
+      <div>
+      {ctime}
+      </div>
     </div>
   );
 }
